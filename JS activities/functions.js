798784b8b35324561cf2cd5b2c869ba5c7fdf49f
Alan Macabuhay <amac714@yahoo.com>
2017-04-18 10:05:08 -0700

    added activity2
    JS activity 2: functions

---
+++ JS activities/functions.js
@@ -0,0 +1,16 @@
+/**
+ * Created by airmac on 4/18/17.
+ */
+
+console.log(doubleNum(23));
+
+function doubleNum(x) {
+
+    return 2 * x;
+}
+
+sayHello();
+
+var sayHello = function () {
+    console.log("Hello World");
+};
