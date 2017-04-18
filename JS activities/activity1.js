b794501ba814b81523a6caddfeeab66d66f6c2c2
Alan Macabuhay <amac714@yahoo.com>
2017-04-18 08:56:27 -0700

    added activity 1
    making sure JS works

---
+++ JS activities/activity1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by airmac on 4/18/17.
+ */
+console.log("Lets make sure JavaScript is working.");
+var name = "Alan"; //Replace this with your first name
+console.log("The unicode characters of your name are:")
+for (var i = 0; i < name.length; i++){
+    console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.")
